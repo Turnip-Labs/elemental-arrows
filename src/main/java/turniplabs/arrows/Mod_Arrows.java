f9fedc19fb60e6862f8371d014e6c25cf453f571
FatherCheese <ythojim@gmail.com>
2022-11-10 17:18:01 -0700

    Elemental Arrows v1.2
    Removed the Homing Bow for now (caused crash)

--- src/main/java/turniplabs/arrows/Mod_Arrows.java
+++ src/main/java/turniplabs/arrows/Mod_Arrows.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.src.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.arrows.item.ItemHomingBow;
 import turniplabs.halplibe.helper.RecipeHelper;
 import turniplabs.halplibe.helper.TextureHelper;
 
@@ -25,7 +24,6 @@ public class Mod_Arrows implements ModInitializer {
     public static final Item arrowFire = new Item(802).setIconCoord(18,0).setItemName(name("ammo.fire"));
     public static final Item arrowIce = new Item(803).setIconCoord(19,0).setItemName(name("ammo.ice"));
     public static final Item arrowLightning = new Item(804).setIconCoord(20,0).setItemName(name("ammo.lightning"));
-    public static final Item homingBow = new ItemHomingBow(805).setIconCoord(21,0).setItemName(name("bow.homing"));
 
     @Override
     public void onInitialize() {
@@ -43,6 +41,5 @@ public class Mod_Arrows implements ModInitializer {
         RecipeHelper.Crafting.createRecipe(arrowFire,4, new Object[]{"1", "2", "3", '1', coal, '2', stick, '3', featherChicken});
         RecipeHelper.Crafting.createRecipe(arrowIce,4, new Object[]{"1", "2", "3", '1', ammoSnowball, '2', stick, '3', featherChicken});
         RecipeHelper.Crafting.createRecipe(arrowLightning,4, new Object[]{"1", "2", "3", '1', diamond, '2', stick, '3', featherChicken});
-        RecipeHelper.Crafting.createRecipe(homingBow,1, new Object[]{" 12", "1 2", " 12", '1', diamond, '2', string});
     }
 }
